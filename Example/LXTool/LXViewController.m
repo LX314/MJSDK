5e4b2db9f10dc57215b9d3a7b6e7f676b3ee346e
张好杰 haojie.zhang <haojie.zhang@weimob.com>
2016-05-17 09:24:42 +0800

    Initial commit

---
+++ Example/LXTool/LXViewController.m
@@ -0,0 +1,29 @@
+//
+//  LXViewController.m
+//  LXTool
+//
+//  Created by 张好杰 haojie.zhang on 05/17/2016.
+//  Copyright (c) 2016 张好杰 haojie.zhang. All rights reserved.
+//
+
+#import "LXViewController.h"
+
+@interface LXViewController ()
+
+@end
+
+@implementation LXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
