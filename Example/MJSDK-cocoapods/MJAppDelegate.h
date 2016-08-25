cf703972dd21264c593136cbee2bfea5e5b1844c
LX314 <1094426094@qq.com>
2016-08-25 14:40:04 +0800

    Initial commit

---
+++ Example/MJSDK-cocoapods/MJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MJAppDelegate.h
+//  MJSDK-cocoapods
+//
+//  Created by LX314 on 08/25/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
