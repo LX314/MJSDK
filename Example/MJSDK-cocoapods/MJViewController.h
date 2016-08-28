1a7bc9f0c3137aa7198d136d1cd9785580f6f6e2
LX314 <1094426094@qq.com>
2016-08-28 10:08:11 +0800

    Initial commit

---
+++ Example/MJSDK-cocoapods/MJViewController.h
@@ -0,0 +1,13 @@
+//
+//  MJViewController.h
+//  MJSDK-cocoapods
+//
+//  Created by LX314 on 08/28/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MJViewController : UIViewController
+
+@end
---
+++ Example/MJSDK-cocoapods/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MJSDK-cocoapods
+//
+//  Created by LX314 on 08/28/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+@import UIKit;
+#import "MJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MJAppDelegate class]));
+    }
+}
