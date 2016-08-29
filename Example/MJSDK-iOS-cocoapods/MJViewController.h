f34231fc3a93203328bc04007d3c24b8574be743
LX314 <1094426094@qq.com>
2016-08-29 17:12:27 +0800

    Initial commit

---
+++ Example/MJSDK-iOS-cocoapods/MJViewController.h
@@ -0,0 +1,13 @@
+//
+//  MJViewController.h
+//  MJSDK-iOS-cocoapods
+//
+//  Created by LX314 on 08/29/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MJViewController : UIViewController
+
+@end
---
+++ Example/MJSDK-iOS-cocoapods/MJViewController.m
@@ -0,0 +1,29 @@
+//
+//  MJViewController.m
+//  MJSDK-iOS-cocoapods
+//
+//  Created by LX314 on 08/29/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+#import "MJViewController.h"
+
+@interface MJViewController ()
+
+@end
+
+@implementation MJViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
